fb37149739d7f5bb1fafabbd698833afacaa68f3
Shaikh Abdul Sami <108601988+shaikh-piaic@users.noreply.github.com>
2023-09-25 16:58:47 +0500

    Created Movies Model

---
+++ server/model/movie.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const movieSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    genre: [{ type: String, ref: "Genre" }],
+    runtime: {
+        type: String,
+        required: true
+    },
+}, { timestamps: true })
+
+const Movies = mongoose.model("Movies", movieSchema)
+export default Movies
